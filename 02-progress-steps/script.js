78c84d7bae0e10079f83eedf52b720b42c0cc6e4
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-18 23:44:26 -0500

    Se resolvio el 2do reto Progress-Steps

---
+++ 02-progress-steps/script.js
@@ -0,0 +1,36 @@
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+const barCharge = document.querySelector('.bar-charge');
+const numbers = [...document.querySelectorAll('.circle')];
+
+let charge = 100;
+let position = 3;
+
+const activatedButtons = (position) => {    
+    if(position === 3) {
+        next.classList.add('isPossible');
+        prev.classList.remove('isPossible');
+    }else if(position < 3 && position >= 1) {
+        prev.classList.add('isPossible');
+        next.classList.add('isPossible');
+    }else {
+        prev.classList.add('isPossible');
+        next.classList.remove('isPossible');
+    }
+}
+
+next.addEventListener('click', () => {
+    if(position === 0) return;
+    barCharge.style.transform = `translateX(-${33*(--position)}%)`;
+    activatedButtons(position);
+    numbers[3-position].classList.add('isActive');
+});
+
+prev.addEventListener('click', () => {
+    if(position === 3) return;
+    barCharge.style.transform = `translateX(-${33*(++position)}%)`;
+    activatedButtons(position);
+    numbers[4-position].classList.remove('isActive');
+});
+

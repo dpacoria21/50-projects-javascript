59e54d60560aa4165025c24d4a93caf7a45c6541
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-21 00:28:05 -0500

    Se va a resolver el mismo problema pero, utilizando Intersection Observer

--- 06-scroll-animation/script.js
+++ 06-scroll-animation/script.js
@@ -3,21 +3,26 @@ const container = document.querySelector('.container');
 
 window.addEventListener('scroll', () => {
 
-    const {availHeight: height} = window.screen;
-
+    
+    
     let side = '';
     let hiddenSide = true;
+    
     elements.forEach((element) => {
-        
+
+        console.log(hiddenSide);        
+        const {availHeight: height} = window.screen;
+
         const hiddenTop = element.getBoundingClientRect().y;
         const hiddenBottom = element.getBoundingClientRect().bottom;
+
         hiddenSide ? side = 'hidden-card-left': side ='hidden-card-right';
-        if(hiddenTop <= 0 || hiddenBottom > height) {
+        
+        if(hiddenTop <= 0 || hiddenBottom >= height) {
             element.classList.add(side);
             element.classList.remove('show-card');
         }else {
             element.classList.add('show-card');
-            console.log(side);
             element.classList.remove(side);
         }
         hiddenSide = !hiddenSide;

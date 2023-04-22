1b0e15fc346beaa346ad86a9637172435121f6d0
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-21 20:20:45 -0500

    Se realizo el mismo ejercicio pero usando IntersectionObserver para evitar el llamado excesivo de la accion scroll, y se mejora el rendimiento

--- 06-scroll-animation/script2.js
+++ 06-scroll-animation/script2.js
@@ -1,26 +1,60 @@
+// const cards = document.querySelectorAll('.card');
+// const hidden = true;
+
+// function callback(entries, observer) {
+//     console.log(entries);
+//     entries.forEach((entry) => {
+//         console.log('Hola bb!');
+//         if(entry.isIntersecting) {
+//             console.log('Elemento visible');
+//         }else {
+//             console.log('No es visible su elemento');
+//         }
+//     });
+// }
+
+// const options = {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: 1
+// };
+
+// const observer = new IntersectionObserver(callback, options);
+
+// cards.forEach((card) => {
+//     observer.observe(card);
+// })
+
 const cards = document.querySelectorAll('.card');
-const hidden = true;
+let hidden = true;
+
+cards.forEach((card) => {
+    let hiddenSide = hidden ? 'hidden-card-left' : 'hidden-card-right'
+    card.classList.add(hiddenSide);
+    hidden = !hidden;
+})
+
 
 function callback(entries, observer) {
-    console.log(entries);
     entries.forEach((entry) => {
-        console.log('Hola bb!');
         if(entry.isIntersecting) {
-            console.log('Elemento visible');
+            console.log('Esta dentro uwu', entry.target);
+            entry.target.classList.add('show-card');
         }else {
-            console.log('No es visible su elemento');
+            console.log('Salio de la pantalla')
+            entry.target.classList.remove('show-card');
         }
-    });
+    })
 }
 
 const options = {
     root: null,
-    rootMargin: '0px',
-    threshold: 1
-};
+    rootMargin: "0px",
+    threshold: 1,
+}
 
 const observer = new IntersectionObserver(callback, options);
 
-cards.forEach((card) => {
+cards.forEach(card => {
     observer.observe(card);
 })

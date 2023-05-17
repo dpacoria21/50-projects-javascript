0f1c95e84984c22dc50279bca2b6733d3490e37b
Diego Pacori <dpacoria@unsa.edu.pe>
2023-05-17 14:38:57 -0500

    Se implemento el contador para las redes

---
+++ 15-incrementing-counter/script.js
@@ -0,0 +1,16 @@
+const views = [12000, 8000, 15000];
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter, i) => {
+    let time=0;
+    const count = setInterval(() => {
+        if(time>=views[i]) {
+            clearInterval(count);
+        } 
+        counter.textContent = time;
+        time+=10;
+    }, 1)
+})
+
+

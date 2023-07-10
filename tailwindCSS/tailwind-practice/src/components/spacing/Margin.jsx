87a1525d63ce81c349f61b6ef831b7bac3dc8272
Diego Pacori <dpacoria@unsa.edu.pe>
2023-07-10 00:22:54 -0500

    Se practico los padding en Tailwind CSS

---
+++ tailwindCSS/tailwind-practice/src/components/spacing/Margin.jsx
@@ -0,0 +1,5 @@
+export const Margin = () => {
+  return (
+    <div>margin</div>
+  )
+}
--- tailwindCSS/tailwind-practice/src/main.jsx
+++ tailwindCSS/tailwind-practice/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { TextApp } from './components/texts/TextApp'
+import { Spacing } from './components/spacing/Spacing'
+// import { TextApp } from './components/texts/TextApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
-      <TextApp />
+      <Spacing />
     </>
   </React.StrictMode>,
 )
